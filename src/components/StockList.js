349696597d931c3fe5c59a7c710d3e85aa3d693e
mailliwmogollon <mailliwmogollon@gmail.com>
2022-01-10 09:14:16 -0400

    add setprices

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -1,12 +1,11 @@
 import './Style.css';
 
 //Hooks
-import { useState} from 'react'; 
+import { useState} from 'react';
 
 export default function StockList() {
-
   const [stock, setStock] = useState("");
-  const [output, setOutput] = useState("");
+  const [prices, setPrices] = useState([]);
 
   const url = "https://finnhub.io/api/v1/quote?";
   const token = "c7be732ad3ia366fsgg0";
@@ -17,44 +16,43 @@ export default function StockList() {
   };
 
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+const fetchUse = async (stock) => {
+      fetch(stockURL(stock))
 
-    fetch(stockURL(stock))
+        .then((response) => response.json())
+        
+        .then((data) => {
+          let price = data.c;
+          const newprice = `${stock.toUpperCase()} price ${price}`
+          setPrices(prices => [...prices, newprice]);
+          }, [setPrices]);
 
-      .then((response) => response.json())
-      
-      .then((data) => {
-        let price = data.c;
-        const stocks = `${stock.toUpperCase()} price ${price} - `
-        const newstocks = output.concat(`${stocks}`)
-        setOutput(newstocks);
-        })
-  
-        .catch((event) => alert("There is something wrong with the server"));
-      
-        setStock("");
-
-      }
+        }
+ const handleSubmit = async (event) => {
+          event.preventDefault();
+          fetchUse(stock)
+          setStock("");
+            }
 
   return (
     <div className="container">
-      <form  onSubmit={handleSubmit} className='form-container'>
-      <input
-         placeholder="Stock Name"
-         type="search"
-         aria-label="Search stock name"
-         value={stock}
-         onInput={(event) => setStock(event.target.value)}
-         className='input-form-container'
+      <form onSubmit={handleSubmit}  className='form-container'>
+        <input
+          placeholder="Stock Name"
+          type="search"
+          aria-label="Search stock name"
+          value={stock}
+          onInput={(event) => setStock(event.target.value)}
+          className='input-form-container'
         />
-      <button className='button-form-container'>
-        Go!
-      </button>
-      <ul id="output" className='ul-form-container'>
-      {output}
-      </ul>
+        <button className='button-form-container'>
+          Go
+        </button>
       </form>
+        {prices?.length > 0 ? <ul id="prices" className='ul-form-container'>
+          {prices.map((price, index) => <li key={index}>{price}</li>)}
+        </ul> : null}
     </div>
   );
 }
+
